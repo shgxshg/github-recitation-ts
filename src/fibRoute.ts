82324d9f21b8e2506b3c47c8ba306768b6e046e1
Deema <dmohanad@andrew.cmu.edu>
2024-08-31 23:59:10 +0300

    fixed to a number instead of any

--- src/fibRoute.ts
+++ src/fibRoute.ts
@@ -1,10 +1,27 @@
-// Endpoint for querying the fibonacci numbers
+// // Endpoint for querying the fibonacci numbers
 
-// with this snippet from fibRoute.ts:
+// const fibonacci = require("./fib");
+
+// import { Request, Response } from "express";
+
+// export default (req: Request, res: Response) => {
+//   const { num } = req.params;
+
+//   const fibN = fibonacci(parseInt(num));
+//   let result = `fibonacci(${num}) is ${fibN}`;
+
+//   if (fibN < 0) {
+//     result = `fibonacci(${num}) is undefined`;
+//   }
+
+//   res.send(result);
+// };
+
+import fibonacci from "./fib";
+
 import { Request, Response } from "express";
-const fibonacci = require("./fib");
 
-export default (req, res) => {
+export default (req: Request, res: Response) => {
   const { num } = req.params;
 
   const fibN = fibonacci(parseInt(num));
