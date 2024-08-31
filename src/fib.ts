ec6cc173df55d2b56253f5c6a5a0d889d4f587e9
shgxshg <129880452+shgxshg@users.noreply.github.com>
2024-09-01 00:26:42 +0300

    fib.ts

--- src/fib.ts
+++ src/fib.ts
@@ -1,12 +1,20 @@
 
 
 export default function fibonacci(n: number): number {
-  
   if (n < 0) {
-    return -1;
-  } else if (n == 0) {
+    return -1; 
+  } else if (n === 0) {
     return 0;
-  } else if (n == 1) {
+  } else if (n === 1) {
     return 1;
+  } else {
+
+    let a = 0, b = 1;
+    for (let i = 2; i <= n; i++) {
+      const temp = a + b;
+      a = b;
+      b = temp;
+    }
+    return b;
   }
 }
