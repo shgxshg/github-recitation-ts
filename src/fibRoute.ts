ea1f1d257dc387dd480f45cb4c82cfc731961f94
Deema <dmohanad@andrew.cmu.edu>
2024-08-31 23:39:40 +0300

    fixed to a number instead of any

--- src/fibRoute.ts
+++ src/fibRoute.ts
@@ -1,41 +1,19 @@
-// // Endpoint for querying the fibonacci numbers
-
-// const fibonacci = require("./fib");
-
-// export default (req, res) => {
-//   const { num } = req.params;
-
-//   const fibN = fibonacci(parseInt(num));
-//   let result = `fibonacci(${num}) is ${fibN}`;
-
-//   if (fibN < 0) {
-//     result = `fibonacci(${num}) is undefined`;
-//   }
-
-//   res.send(result);
-// };
-
 // Endpoint for querying the fibonacci numbers
-import { Request, Response } from 'express';
-import fibonacci from './fib';  // Assuming your fibonacci function is the default export from fib
 
-export default (req: Request, res: Response): void => {
-  const num = parseInt(req.params.num, 10);  // Parse `num` from request parameters
+// with this snippet from fibRoute.ts:
+import { Request, Response } from "express";
+const fibonacci = require("./fib");
 
-  // Check if `num` is a valid number
-  if (isNaN(num)) {
-    res.status(400).send('Invalid number');
-    return;
-  }
+export default (req, res) => {
+  const { num } = req.params;
 
-  const fibN = fibonacci(num);  // Call the fibonacci function
-  let result: string;
+  const fibN = fibonacci(parseInt(num));
+  let result = `fibonacci(${num}) is ${fibN}`;
 
   if (fibN < 0) {
     result = `fibonacci(${num}) is undefined`;
-  } else {
-    result = `fibonacci(${num}) is ${fibN}`;
   }
 
   res.send(result);
 };
+
